fdde484377fa5938acc6fc38116dfdf5c2b260e6
AmeerSalahaldeen <Ameer@Mac-5-MacBook-Pro.local>
2016-03-15 17:45:19 +0200

    f

---
+++ src/Expedia/Facade.php
@@ -0,0 +1,12 @@
+<?php namespace HotelsProviders\Expedia;
+
+class Facade extends \Illuminate\Support\Facades\Facade {
+
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor() { return 'yamsafer-providers.expedia'; }
+
+}
---
+++ src/Expedia/ServiceProvider.php
@@ -0,0 +1,52 @@
+<?php namespace HotelsProviders\Expedia;
+
+use Illuminate\Foundation\AliasLoader;
+
+class ServiceProvider extends \Illuminate\Support\ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+        // Register the package namespace
+        $this->package('HotelsProviders\Expedia');
+
+		// Auto create app alias with boot method.
+		AliasLoader::getInstance()->alias('ExpediaProviders', 'HotelsProviders\Expedia\Facade');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		$this->app['HotelsProvider.expedia'] = $this->app->share(function($app)
+		{
+            $config = $app->config->get('HotelsProviders::config', array());
+
+			return new ExpediaProvider($config);
+		});
+	}
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
+}
